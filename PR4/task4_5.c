6915c172fdf6abd27f327ebd6487607059a3b50f
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 01:43:56 +0300

    Update task4_5.c

--- PR4/task4_5.c
+++ PR4/task4_5.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
   #include <stdlib.h>
+  #include <sys/resource.h>
 
   int main() {
-      void *ptr = NULL;
-      int i = 0;
-      while (i < 3) {
-          if (!ptr) {
-              ptr = malloc(100);
-              if (ptr == NULL) {
-                  printf("Помилка виділення пам’яті\n");
-                  return 1;
-              }
-          }
-          printf("Використання ptr: %p\n", ptr);
-          i++;
+      struct rlimit rl;
+      getrlimit(RLIMIT_AS, &rl);
+      rl.rlim_cur = 1024 * 1024;
+      setrlimit(RLIMIT_AS, &rl);
+      void *ptr = malloc(512 * 1024);
+      if (ptr == NULL) {
+          printf("Помилка початкового malloc\n");
+          return 1;
       }
-      free(ptr); // Звільнення лише один раз
+      void *new_ptr = realloc(ptr, 2 * 1024 * 1024); 
+      if (new_ptr == NULL) {
+          printf("realloc не зміг виділити 2 МБ, ptr збережено: %p\n", ptr);
+      } else {
+          printf("realloc успішно: %p\n", new_ptr);
+          ptr = new_ptr;
+      }
+      free(ptr);
       return 0;
   }
