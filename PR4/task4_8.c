e43807c6a193a3e54afa4062d7bd3a1f0d39fe5f
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 02:02:35 +0300

    Update task4_8.c

--- PR4/task4_8.c
+++ PR4/task4_8.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
   #include <stdlib.h>
   #include <pthread.h>
+  #include <unistd.h> 
 
   void *thread_func(void *arg) {
-      void *ptr = malloc(100);
+      void *ptr = malloc(100); 
       if (ptr == NULL) {
           printf("Потік: Помилка malloc\n");
       } else {
           printf("Потік %ld: Пам’ять виділено: %p\n", (long)arg, ptr);
       }
-      sleep(1);
+      sleep(1); 
       free(ptr);
       return NULL;
   }
 
   int main() {
-      pthread_t threads[5];
+      pthread_t threads[5]; 
       for (long i = 0; i < 5; i++) {
           pthread_create(&threads[i], NULL, thread_func, (void *)i);
       }
       for (int i = 0; i < 5; i++) {
-          pthread_join(threads[i], NULL);
+          pthread_join(threads[i], NULL); 
       }
       return 0;
   }
