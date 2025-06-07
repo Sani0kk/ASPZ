3309bd27d6f1674642efde8274ef21eb7ea7ffe1
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 01:42:45 +0300

    Create task4_5.c

---
+++ PR4/task4_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+  #include <stdlib.h>
+
+  int main() {
+      void *ptr = NULL;
+      int i = 0;
+      while (i < 3) {
+          if (!ptr) {
+              ptr = malloc(100);
+              if (ptr == NULL) {
+                  printf("Помилка виділення пам’яті\n");
+                  return 1;
+              }
+          }
+          printf("Використання ptr: %p\n", ptr);
+          i++;
+      }
+      free(ptr); // Звільнення лише один раз
+      return 0;
+  }
