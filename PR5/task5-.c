b7bcd3d5f44d4cfc84b5bec82f664565a0c7c805
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-07 11:02:12 +0300

    Update task5-.c

--- PR5/task5-.c
+++ PR5/task5-.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int x = 12345;
+
+    // Помилково використали x як покажчик
+    int value = * (int *) x;
+
+    printf("Value = %d\n", value);
+    return 0;
+}
