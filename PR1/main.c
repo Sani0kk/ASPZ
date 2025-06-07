9df3881d628417f2050a2e2f5419ab00e7c4227e
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 00:34:27 +0300

    Update main.c

--- PR1/main.c
+++ PR1/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-void input();
-void output();
-void process();
+#include "input.h"
+#include "output.h"
+#include "process1.h"
+#include "process.h"
+
 int main() {
-    input();
-    process();
-    output();
+    int value = get_input();
+    int processed1 = process1(value);
+    int processed2 = process(value);
+    print_output(processed1, processed2);
     return 0;
 }
