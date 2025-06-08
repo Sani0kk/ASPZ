9366913c716dcc479ff826acf205421ea5900c88
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 05:31:32 +0300

    Update task14_1.c

--- PR14/task14_1.c
+++ PR14/task14_1.c
@@ -8,14 +8,22 @@ volatile sig_atomic_t counter = 0;
 
 void timer_handler(int signum) {
     counter++;
-    printf("Лічильник: %ld\n", (long)counter); // Використано %ld для long int
-    fflush(stdout); // Примусове виведення
+    printf("Лічильник: %d\n", counter);
+
+    if (counter >= 50) {
+        // Вимикаємо таймер
+        struct itimerval stop = {{0, 0}, {0, 0}};
+        setitimer(ITIMER_REAL, &stop, NULL);
+        printf("Таймер вимкнено після 5 секунд.\n");
+        exit(0); // Завершити програму
+    }
 }
 
 int main() {
     struct sigaction sa;
     struct itimerval timer;
 
+    // Встановлення обробника сигналу SIGALRM
     sa.sa_handler = timer_handler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
@@ -24,27 +32,21 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Налаштування таймера: 100 мс інтервал
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 100000; // Початковий інтервал 100 мс
+    timer.it_value.tv_usec = 100000;
     timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 100000; // Періодичне спрацьовування 100 мс
+    timer.it_interval.tv_usec = 100000;
 
     if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
         perror("setitimer");
         exit(EXIT_FAILURE);
     }
 
-    sleep(5);
-
-    timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 0;
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 0;
-    if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
-        perror("disable timer");
-        exit(EXIT_FAILURE);
+    // Цикл очікування сигналів
+    while (1) {
+        pause(); // Чекає сигналів
     }
 
-    printf("Таймер вимкнено після 5 секунд.\n");
     return 0;
 }
