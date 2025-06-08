c762d7f6480767ebad870184a96bd3d28a836d43
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 12:09:37 +0300

    Update task9_2.c

--- PR9/task9_2.c
+++ PR9/task9_2.c
@@ -3,18 +3,9 @@
 #include <unistd.h>
 
 int main() {
-    if (setuid(0) == -1) {
-        fprintf(stderr, "Помилка: не вдалося отримати права root. Перевірте sudo.\n");
-        exit(EXIT_FAILURE);
-    }
-    if (getuid() != 0) {
-        fprintf(stderr, "Помилка: немає доступу до root.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    FILE *fp = popen("cat /etc/shadow", "r");
+    FILE *fp = popen("sudo cat /etc/master.passwd", "r");
     if (fp == NULL) {
-        perror("Помилка виконання popen");
+        perror("Помилка при виконанні popen");
         exit(EXIT_FAILURE);
     }
 
