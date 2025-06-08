e3c81a920dad592a60c25b83d46d33ce28aafc7c
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 05:36:22 +0300

    Update task14_1.c

--- PR14/task14_1.c
+++ PR14/task14_1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
 #include <sys/time.h>
-#include <unistd.h>
 
 volatile sig_atomic_t counter = 0;
 
-void timer_handler(int signum) {
+void handler(int sig) {
     counter++;
     printf("Лічильник: %d\n", counter);
+    fflush(stdout);
 
     if (counter >= 50) {
-        struct itimerval stop = {{0, 0}, {0, 0}};
-        setitimer(ITIMER_REAL, &stop, NULL);
         printf("Таймер вимкнено після 5 секунд.\n");
+        fflush(stdout);
         exit(0);
     }
 }
@@ -22,29 +22,27 @@ int main() {
     struct sigaction sa;
     struct itimerval timer;
 
-    sa.sa_handler = timer_handler;
-    sa.sa_flags = 0;
+    sa.sa_handler = handler;
     sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART; 
 
     if (sigaction(SIGALRM, &sa, NULL) == -1) {
         perror("sigaction");
         exit(EXIT_FAILURE);
     }
 
-
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 100000;      // перший запуск через 100 мс
+    timer.it_value.tv_usec = 100000;  // 100 мс
     timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 100000;   // повторення кожні 100 мс
+    timer.it_interval.tv_usec = 100000;
 
     if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
         perror("setitimer");
         exit(EXIT_FAILURE);
     }
 
-    // Чекаємо сигналів у циклі
     while (1) {
-        pause();
+        usleep(1000); 
     }
 
     return 0;
