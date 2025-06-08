69515694b57d0902ff80fa5db0828fe73582cab4
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 05:33:19 +0300

    Update task14_1.c

--- PR14/task14_1.c
+++ PR14/task14_1.c
@@ -11,11 +11,10 @@ void timer_handler(int signum) {
     printf("Лічильник: %d\n", counter);
 
     if (counter >= 50) {
-        // Вимикаємо таймер
         struct itimerval stop = {{0, 0}, {0, 0}};
         setitimer(ITIMER_REAL, &stop, NULL);
         printf("Таймер вимкнено після 5 секунд.\n");
-        exit(0); // Завершити програму
+        exit(0);
     }
 }
 
@@ -23,29 +22,29 @@ int main() {
     struct sigaction sa;
     struct itimerval timer;
 
-    // Встановлення обробника сигналу SIGALRM
     sa.sa_handler = timer_handler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
+
     if (sigaction(SIGALRM, &sa, NULL) == -1) {
         perror("sigaction");
         exit(EXIT_FAILURE);
     }
 
-    // Налаштування таймера: 100 мс інтервал
+
     timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 100000;
+    timer.it_value.tv_usec = 100000;      // перший запуск через 100 мс
     timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 100000;
+    timer.it_interval.tv_usec = 100000;   // повторення кожні 100 мс
 
     if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
         perror("setitimer");
         exit(EXIT_FAILURE);
     }
 
-    // Цикл очікування сигналів
+    // Чекаємо сигналів у циклі
     while (1) {
-        pause(); // Чекає сигналів
+        pause();
     }
 
     return 0;
