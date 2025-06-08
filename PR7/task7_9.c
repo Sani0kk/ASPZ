472247bc797ee6b628437b6fc88e79a9c733648d
Sani0kk <136684090+Sani0kk@users.noreply.github.com>
2025-06-08 03:16:03 +0300

    Create task7_9.c

---
+++ PR7/task7_9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+  #include <time.h>
+
+  int main() {
+      clock_t start = clock(); // Початок часу
+      for (int i = 0; i < 1000000; i++) {} // Довгий цикл
+      clock_t end = clock(); // Кінець часу
+      double time_spent = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+      printf("Час виконання: %.2f мс\n", time_spent);
+      return 0;
+  }
